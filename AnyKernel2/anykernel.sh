33a896cb4d74ac264c883d0ced98bb8650801aca
Albert Tang <tangalbert919@gmail.com>
2018-03-01 21:34:40 -0600

    Make sure AnyKernel2 works with all hlte variants

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -4,20 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=DirtyV by bsmitty83 @ xda-developers
+kernel.string=ZyborgX by jprimero15 and andr01dfr3ak @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
-device.name5=
+device.name1=hlte
+device.name2=hltekor
+device.name3=hltetmo
+device.name4=hltespr
+device.name5=hltevzw
+device.name6=hltexx
+device.name7=hlteusc
+device.name8=hltechn
 } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -44,16 +47,6 @@ replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl c
 append_file init.rc "run-parts" init;
 
 # init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 # end ramdisk changes
 
